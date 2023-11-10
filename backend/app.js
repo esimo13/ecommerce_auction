1895f2fa20e57488e2a154f06d4852e6704dffd5
esimo13 <obak139@gmail.com>
2023-11-10 14:52:08 +0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const cors = require("cors");
+
+const errorMiddleware = require("./middleware/error");
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests only from this origin
+    methods: "GET,POST", // Allow only specified HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Allow only specified headers
+    optionsSuccessStatus: 204, // Respond with 204 No Content for preflight requests
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+// Route Imports
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+module.exports = app;
