eed1d111c85a48aecd417d07bb4b9e6325b5dd0b
esimo13 <obak139@gmail.com>
2023-11-17 19:34:46 +0600

    fix time

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,6 +1,8 @@
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(
+  "sk_test_51ODS0PJJB9jyYs2sTpheEkgU7bpDx5g4AjbI8FH9zvr7tixo5H2heNmRlueS6ME298y4pNip0A6QRbViI2TRDmKs00glOSjU6V"
+);
 
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
@@ -17,5 +19,8 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+  res.status(200).json({
+    stripeApiKey:
+      "pk_test_51ODS0PJJB9jyYs2sP6cdsDXelJqblf3p4W9VsLsPGflUSTiGhLOoxUXdPSoar0J5zAXe8TUZTJhgXh2tC96xCTyM00VfMLxJ5G",
+  });
 });
